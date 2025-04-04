103f0ada620638860201974cd1d8f8505bd1fc84
nombrarnaud <133082129+nombrarnaud@users.noreply.github.com>
2025-04-04 19:17:35 -0400

    Add src/App.vue 1743808654693

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <hero-component />
+    <ticket-form-component />
+    <footer-component />
+  </div>
+</template>
+
+<script>
+import Hero from './components/Hero.vue'
+import TicketReservationForm from './components/TicketReservationForm.vue'
+import EventFooter from './components/EventFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'hero-component': Hero,
+    'ticket-form-component': TicketReservationForm,
+    'footer-component': EventFooter
+  }
+}
+</script>
+
+<style>
+/* Global styles */
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
